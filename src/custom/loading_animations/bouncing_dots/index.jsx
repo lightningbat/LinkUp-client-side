ef882e6c6be461010f1932073e8b2cc1332942f7
Dash <missionredelephant@gmail.com>
2024-09-19 14:07:20 +0530

    completed custom bouncing dots animation

--- src/custom/loading_animations/bouncing_dots/index.jsx
+++ src/custom/loading_animations/bouncing_dots/index.jsx
@@ -1,11 +1,17 @@
 import './style.scss'
 
-export default function BouncingDots() {
+export default function BouncingDots({scale = 1, color = 'var(--primary-text-color)', speed = 1}) {
+    if (scale <= 0 ) scale = 1
+    if (speed <= 0) speed = 1
+    if (!color || color === '') color = 'var(--primary-text-color)'
+
+    const defaultScale = 10 * scale
+    const animation_duration = 0.5 / speed
     return (
-        <div className="loading-animation bouncing-dots">
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="loading-animation bouncing-dots" style={{fontSize: `${defaultScale}px`}}>
+            <div style={{backgroundColor: color, animationDuration: `${animation_duration}s`}}></div>
+            <div style={{backgroundColor: color, animationDuration: `${animation_duration}s`, animationDelay: `${animation_duration * 20 / 100}s`}}></div>
+            <div style={{backgroundColor: color, animationDuration: `${animation_duration}s`, animationDelay: `${animation_duration * 40 / 100}s`}}></div>
         </div>
     )
 }
