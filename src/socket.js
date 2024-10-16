d6233e22ba9613eae4fbf35169dd8917e6d69411
lightningbat <183808230+lightningbat@users.noreply.github.com>
2024-10-16 22:07:43 +0530

    minor changes

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
-import { getToken } from './services/authenticationService';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
